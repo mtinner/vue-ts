d02b4556ce0e01c412a0eab73d323e7e08bafb13
Marcel Tinner <m1tinner@gmail.com>
2017-01-16 14:18:16 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {app: './app.ts',},
+    output: {filename: 'app.js'},
+
+    // resolve TypeScript and Vue file
+    resolve: {
+        extensions: ['', '.ts', '.vue', '.js']
+    },
+
+    module: {
+        loaders: [
+            {test: /\.vue$/, loader: 'vue'},
+            {test: /\.ts$/, loader: 'vue-ts'}
+        ],
+    },
+    vue: {
+        // instruct vue-loader to load TypeScript
+        loaders: {js: 'vue-ts-loader',},
+        // make TS' generated code cooperate with vue-loader
+        esModule: true
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {warnings: false}
+        })
+    ]
+};
